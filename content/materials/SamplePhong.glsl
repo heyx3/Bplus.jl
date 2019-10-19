1d148a85a0864c84e36aa3c977f067c4300abadc
Billy <manning.w27@gmail.com>
2019-10-19 14:50:40 -0400

    Update some stuff after copying

--- content/materials/SamplePhong.glsl
+++ content/materials/SamplePhong.glsl
@@ -1,17 +1,23 @@
+//This is an AUTO-GENERATED temporary GLSL file for editing
+//    as part of the _____ editor.
+//DO NOT modify outside of the marked areas far below;
+//    it won't accomplish anything.
+
 //=========================================
 //           Vertex Shader:
 //=========================================
 #version 440 core
 
 //Graphics settings configuration:
-//General graphics quality level, from 1 to 3:
-#define c_Quality 2
+//General graphics quality level, from 1 to 5:
+uniform uint s_Quality;
 //Lights:
-#define c_MaxDirectionalLights 1
-#define c_MaxPointLights 4
+#define c_MaxDirectionalLights 3
+#define c_MaxPointLights 6
 
-//Features toggled by the material settings:
-#define NORMAL_MAP 1
+//Features toggled by the material/app settings:
+uniform bool s_UseNormalMap = true;
+uniform bool s_HasScreenGrab = false;
 
 //...user configuration goes here...
 
@@ -39,6 +45,7 @@ struct S_Camera {
     float FoV; // For perspective cameras, this is the vertical FoV.
                // For orthographic cameras, this is the vertical view size.
     float Aspect; //Width over height.
+    uvec2 ScreenSize; //Size in pixels of the render target/screen.
     float NearClip, FarClip;
 };
 struct S_Light_Params {
@@ -64,21 +71,18 @@ uniform S_Light_Point       u_Lights_Point[c_MaxPointLights];
 uniform uint                u_Lights_NDirectional, u_Lights_NPoint;
 uniform vec3                u_SurfaceTint = 1.0f;
 
-uniform sampler2D u_DiffuseMap;
-uniform sampler2D u_NormalMap;
-
-
-//Custom definitions:
+uniform sampler2D u_DiffuseMap, u_NormalMap,
+                  u_ScreenColor, u_ScreenDepth;
 
 //Point transformations:
 vec4 H_TransformPoint_Full(mat4 transf, vec3 p) { return transf          * vec4(p, 1.0f); }
 vec3 H_TransformPoint_Fast(mat4 transf, vec3 p) { return (mat4x3(transf) * vec4(p, 1.0f)).xyz; }
-vec3 H_TransformPoint(mat3 transf, vec3 p)      { return transf          * p; }
+vec3 H_TransformPoint     (mat3 transf, vec3 p) { return transf          * p; }
 
 //Vector transformations:
 vec4 H_TransformVector_Full(mat4 transf, vec3 v) { return mat3x4(transf) * v; }
 vec3 H_TransformVector_Fast(mat4 transf, vec3 v) { return mat3x3(transf) * v; }
-vec3 H_TransformVector(mat3 transf, vec3 v)      { return transf         * v; }
+vec3 H_TransformVector     (mat3 transf, vec3 v  { return transf         * v; }
 
 //Normal-map helpers:
 vec3 H_UnpackNormalMap(vec4 map)                                        { return -1.0f + (2.0f * map.rgb); }
@@ -167,9 +171,11 @@ in vec3 vOut_WorldNormal;
 in vec3 vOut_WorldTangent;
 in vec3 vOut_WorldBitangent;
 
-out vec4 fOut_Color;
+out vec4 fOut;
 
 //...same defines/uniforms as in the vertex shader...
+vec2 GetScreenUV(vec2 pixelPos) { return pixelPos / vec2(u_Camera.ScreenSize); }
+vec2 GetScreenUV() { return GetScreenUV(gl_fragCoord.xy); }
 
 
 void main()
@@ -180,50 +186,53 @@ void main()
     vec3 fIn_WorldNormal = vOut_WorldNormal;
     vec3 fIn_WorldTangent = vOut_WorldTangent;
     vec3 fIn_WorldBitangent = vOut_WorldBitangent;
+    vec2 fIn_ScreenUV = GetScreenUV();
 
     //Module: Setup
     //--------
 
     vec3 fIn_TrueWorldNormal = fIn_WorldNormal;
-    #if NORMAL_MAP
-    //Module: NormalMapping
-    fIn_WorldNormal = H_TransformVector(H_GetNormalMapTransform(),
-                                        H_UnpackNormalMapN(texture(u_NormalMap, fIn_UV)));
-    //--------
-    #endif
+    if (s_UseNormalMap)
+    {
+        //Module: NormalMapping
+        fIn_WorldNormal = H_TransformVector(H_GetNormalMapTransform(),
+                                            H_UnpackNormalMapN(texture(u_NormalMap, fIn_UV)));
+        //--------
+    }
 
     //Module: SetupPostNormalMap
     //--------
     
     //Module: LightGather
-    vec3 fragToCamDir = normalize(u_Camera.Pos - fIn_WorldPos);
-    vec3 totalLight = 0.0f;
+    vec3 v_fragToCamDir = normalize(u_Camera.Pos - fIn_WorldPos);
+    vec3 v_totalLight = 0.0f;
     //Module: DirectionalLightGather
     for (int i = 0; i < u_Lights_NDirectional; ++i)
-        totalLight += H_GetLightColor(u_Lights_Directional[i],
-                                      fIn_WorldPos, fIn_WorldNormal, fragToCamDir);
+        v_totalLight += H_GetLightColor(u_Lights_Directional[i],
+                                        fIn_WorldPos, fIn_WorldNormal, v_fragToCamDir);
     //--------
     //Module: PointLightGather
     for (int i = 0; i < u_Lights_NPoint; ++i)
-        totalLight += H_GetLightColor(u_Lights_Point[i],
-                                      fIn_WorldPos, fIn_WorldNormal, fragToCamDir);
+        v_totalLight += H_GetLightColor(u_Lights_Point[i],
+                                        fIn_WorldPos, fIn_WorldNormal, v_fragToCamDir);
     //--------
     //--------
     
-    vec3 diffuseColor = u_SurfaceTint;
-    float alpha = 1.0f;
+    vec3 v_diffuseColor = u_SurfaceTint;
+    float v_alpha = 1.0f;
     
     //Module: GetDiffuse
-    vec4 diffuse4 = texture(u_DiffuseMap, fIn_UV);
+    vec4 v_diffuse4 = texture(u_DiffuseMap, fIn_UV);
+    v_diffuseColor *= v_diffuse4.rgb;
+    v_alpha = v_diffuse4.a;
     //--------
-    diffuseColor *= diffuse4.rgb;
-    alpha = diffuse4.a;
 
+    vec3 v_surfaceColor = vec3(1.0f, 0.0f, 1.0f);
     //Module: ApplyLight
-    vec3 surfaceColor = totalLight * diffuseColor;
+    v_surfaceColor = v_totalLight * v_diffuseColor;
     //--------
 
+    fOut = vec4(v_surfaceColor, v_alpha);
     //Module: Finalize
-    fOut_Color = vec4(surfaceColor, alpha);
     //--------
 }
