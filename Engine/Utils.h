ed1fad6560b265de73bedfa2e111d70ce9762d69
Billy <manning.w27@gmail.com>
2019-11-29 00:01:12 -0500

    Add Lazy<> utility struct for lazy instantiation of objects

--- Engine/Utils.h
+++ Engine/Utils.h
@@ -3,6 +3,7 @@
 #include "Platform.h"
 #include <functional>
 
+
 //Custom assert macro that can be configured by users of this engine.
 //Only used in debug builds.
 #pragma region BPAssert
@@ -41,6 +42,93 @@ namespace Bplus
 
 //This helper macro escapes commas inside other macro calls.
 #define BP_COMMA ,
+
+
+#pragma region Lazy<> struct, for lazy instantiation of a class
+namespace Bplus
+{
+    //An object that isn't actually constructed until it is needed.
+    template<typename T>
+    struct Lazy
+    {
+    public:
+        using Inner_t = T;
+
+
+        bool IsCreated() const { return isCreated; }
+
+        template<typename... Args>
+        void Create(Args&&... args)
+        {
+            BPAssert(!isCreated, "Already created this!");
+            isCreated = true;
+            new (&itemBytes[0]) T(std::forward<Args>(args)...);
+        }
+
+        
+        //Gets the underlying data, creating it with the default constructor if necessary.
+        const T& Get() const
+        {
+            //If not created already, the instance should be created now.
+            //However, this operation is supposed to be a const op that returns a const instance,
+            //    so we can't avoid some const casting.
+            if (!isCreated)
+                const_cast<Lazy<T>*>(this)->Create();
+
+            return Cast();
+        }
+        T& Get()
+        {
+            return const_cast<T&>(const_cast<const Lazy<T>*>(this)->Get());
+        }
+
+        //Gets the underlying data, assuming it's already been created.
+        const T& Cast() const
+        {
+            BPAssert(isCreated, "Accessed before creation");
+            return *(const T*)(&itemBytes[0]);
+        }
+        T& Cast()
+        {
+            return const_cast<T&>(const_cast<const Lazy<T>*>(this)->Cast());
+        }
+        
+
+        #pragma region constructor, destructor, move, copy
+
+        Lazy() { }
+        ~Lazy()
+        {
+            if (isCreated)
+                Cast().~T();
+            isCreated = false;
+        }
+
+        Lazy(const Lazy<T>& cpy)
+            : isCreated(cpy.isCreated)
+        {
+            if (cpy.isCreated)
+                Create(cpy.Cast());
+        }
+        Lazy(Lazy<T>&& src)
+            : isCreated(src.isCreated)
+        {
+            if (src.isCreated)
+                Create(std::move(src.Cast()));
+        }
+
+        Lazy(const T& cpy) : isCreated(true) { Create(cpy); }
+        Lazy(T&& src)      : isCreated(true) { Create(std::move(src)); }
+
+        #pragma endregion
+
+
+    private:
+        mutable bool isCreated = false;
+        uint8_t itemBytes[sizeof(T)];
+    };
+}
+#pragma endregion
 
 
 #pragma region _strong_typedef helper struct
