660b858e7a71172c989ec0c85bf52926136653b4
heyx3 <manning.w27@gmail.com>
2020-03-21 22:49:10 -0400

    Add default constructors for BETTER_ENUMS

--- Engine/Utils.h
+++ Engine/Utils.h
@@ -2,6 +2,7 @@
 
 #include "Platform.h"
 
+
 //The BETTER_ENUM() macro, to define an enum
 //    with added string conversions and iteration.
 //There is one downside: to use a literal value, it must be prepended with a '+',
@@ -9,6 +10,11 @@
 //#define BETTER_ENUMS_API BP_API
 //NOTE: The #define above was removed because it screws up usage from outside this library,
 //    and I'm pretty sure it's not even needed in the first place.
+//
+//Before including the file, I'm adding a custom modification to enable
+//    default constructors for these enum values:
+#define BETTER_ENUMS_DEFAULT_CONSTRUCTOR(Enum) public: Enum() = default;
+//
 #include <better_enums.h>
 
 
