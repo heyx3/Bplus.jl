83b5e469b101677c4a6240212e590b9826e4b559
heyx3 <manning.w27@gmail.com>
2023-10-05 01:30:02 -0400

    Rewrite bits of `file_cacher.jl` to simplify it

--- src/Helpers/file_cacher.jl
+++ src/Helpers/file_cacher.jl
@@ -48,37 +48,6 @@ mutable struct CachedData{TCached}
     disk_check_interval::Millisecond
 end
 
-"
-Implement this function for your `TCached` data type.
-It should return the loaded instance, optionally paired with an iterator of 'dependent' file paths
-    that can also trigger a reload of this data.
-
-You must overload the first version, but you can also support the second version
-    to re-use resources from a previously-closed instance (see `close_cached_data()`).
-"
-load_uncached_data(T, path) = error("load_uncached_data() not implemented for ", T)
-load_uncached_data(T, path, pooled_resources) = error("load_uncached_data() not implemented for ", T)
-load_uncached_data(T, path, ::Nothing) = load_uncached_data(T, path)
-
-"
-Cleans up the data, and optionally returns stuff to be re-used
-    for new instances (see `load_uncached_data()`).
-This is called 'pooling'.
-
-If you *do* return extra stuff, and that pooled data itself needs cleanup,
-    then you should also overload `close_pooled_data()` to handle a rare case
-    where that stuff doesn't get used for pooling.
-"
-close_cached_data(c) = nothing
-"
-Cleans up 'pooled' data, meaning data returned from `close_cached_data()`
-    in the hopes of re-using it for new instances.
-In rare cases, this pooled data is not re-used, and then this function is called.
-
-The first parameter is the type of the cached data (usually referred to as `TCached`).
-"
-close_pooled_data(T, d) = nothing
-
 function check_disk_modifications!(cd::CachedData)::Bool
     current_datetime = now()
     if (current_datetime - cd.last_check_time) >= cd.disk_check_interval
@@ -93,59 +62,42 @@ end
 ##   FileCacher -- manages a set of CachedData   ##
 
 "
-Keeps track of file data of a certain type and automatically checks it for changes.
+Keeps track of file data of a certain type and automatically checks it for changes. Has the following interface:
 
-To use it, define the following things:
-
-* Some 'TCached' type representing your loaded file data
-* `load_uncached_data(::Type{TCached}, path::AbstractString[, pooled_resources])` to load the file data
-  * Optionally return it in a tuple with a iterator of other dependent files that affect this data
-  * If you want to facilitate object pooling, return some resources when closing an instance (see below) and take those resources as a third parameter.
-* `close_cached_data(c::TCached)` is called when an out-of-date file is cleaned up.
-  * By default, it does nothing.
-  * If you overload this function, you can return some stuff that will get passed to the next `load_uncached_data()` call, allowing you to set up pooling.
+* Update it and check for modified files with `check_disk_modifications!()`.
+    * Ideally, call this every frame.
+    * If at least one file changed, it returns true, and also raises a callback to reload each changed file.
+* Get a file with `get_cached_data!()`. You can provide both relative and absolute paths.
+* Get the canonical representation of a file path within the cacher with `get_cached_path()`.
 
 You can configure the cacher in a few ways:
 
-* 'error_response' is called when a file doesn't exist. It can log and/or return a fallback.
+* `reload_response(path[, old_data]) -> new_data[, dependent_files]` is called when either
+    a file is being loaded for the first time, OR a file has changed and should be reloaded.
+  * Returns the new file, and optionally an iterator of 'dependent' files.
+      whose changes will also trigger a reload.
+  * If your response throws an error then `error_response`, mentioned below, is invoked.
+* `error_response(path, exception, trace[, old_data]) -> [fallback_data]` is called when a file fails to load or re-load.
   * By default it `@error`s and returns `nothing`.
-* 'check_interval_ms' is a range of wait times in between checking files for changes. A wait time is randomly chosen for each file,
+  * You can change it to, for example, return a fallback 'error data' object.
+* `check_interval_ms` is a range of wait times in between checking files for changes. A wait time is randomly chosen for each file,
      to prevent them from all checking the disk at once.
-* 'relative_path' is the path that all files are relative to (except for ones that are given as an absolute path).
+* `relative_path` is the prefix for relative paths.
   * By default, it's the process's current location.
-
-Finally, utilize it with the following:
-
-* `check_disk_modifications!()` to update it, looking for modified files.
-  * Ideally, call this every frame.
-  * It can optionally write into a list of the files that were modified.
-* `get_cached_data!()` to get a given file.
-* `get_cached_path()` to get the canonical representation of a file path within the cacher.
-  * The paths provided in `check_disk_modifications!()` are written in this format, so it's important to know.
 "
-mutable struct FileCacher{TCached}
-    error_response::Base.Callable
-    check_interval_ms::IntervalU
-    relative_path::String
-    files::Dict{AbstractString, CachedData{TCached}} # Stored as their absolute, canonical paths.
-    buffer::Vector{AbstractString} # Used within some functions
+@kwdef mutable struct FileCacher{TCached}
+    reload_response::Base.Callable # (path[, old]) -> new[, dependent_files]
+    error_response::Base.Callable = default_cache_error_response # (path, exception, trace[, old]) -> [new]
 
-    @inline function FileCacher{TCached}(; check_interval_ms = IntervalU(min=3000, max=5000),
-                                           error_response = default_cache_error_response,
-                                           relative_path = pwd()
-                                        ) where {TCached}
-        return new(
-            error_response,
-            convert(IntervalU, check_interval_ms),
-            string(relative_path),
-            Dict{AbstractString, CachedData{TCached}}(),
-            Vector{AbstractString}()
-        )
-    end
+    relative_path::String = pwd()
+    check_interval_ms::IntervalU = 3000:5000
+
+    files::Dict{AbstractString, CachedData{TCached}} = Dict() # Stored as their absolute, canonical paths.
+    buffer::Vector{AbstractString} = [ ] # Used within some functions
 end
 #TODO: Rewrite this so a separate Task constantly checks all the files, sleeping in between each one
 
-function default_cache_error_response(path, exception, trace)
+function default_cache_error_response(path, exception, trace, old_data = nothing)
     @error "Unable to load $path." ex=(exception, trace)
     return nothing
 end
@@ -156,50 +108,24 @@ function get_cached_path(fc::FileCacher, relative_path::AbstractString)::String
         return normpath(fc.relative_path, string(relative_path))
     end
 end
-"
-Attempts to load the given file and its cache metadata.
-Returns `nothing` if it failed.
-
-Optionally takes some pooled resources to give to the data constructor.
-"
+"Loads the given file and its cache metadata"
 function make_data_cache( fc::FileCacher{TCached},
-                          full_path,
-                          pooled_resource...
-                        )::Optional{CachedData{TCached}} where {TCached}
-    # Load the data.
-    (new_data, new_dependent_files) = try
-        loaded = load_uncached_data(TCached, full_path, pooled_resource...)
-        if loaded isa Tuple
-            loaded
-        else
-            (loaded, ())
-        end
-    # If it fails, call the fallback lambda and try to use that instead.
-    catch e
-        close_pooled_data.(Ref(TCached), pooled_resource)
-        (fc.error_response(full_path, e, catch_backtrace()), ())
-    end
-
-    # If we have a file to cache, cache it.
-    if new_data isa TCached
-        return CachedData(
-            new_data,
-            # Get the "last-modified" time for each dependent file, plus the main one.
-            let paths = tuple(full_path, (get_cached_path(fc, p) for p in new_dependent_files)...),
-                file_modified_time(p) = unix2datetime(state(p).mtime)
-                Dict(p=>file_modified_time(p) for p in paths)
-            end,
-            full_path, now(), rand(fc.check_interval_ms)
-        )
-    # Otherwise, its an error case without a valid fallback file. Wipe it out of the cache.
-    else
-        return nothing
-    end
+                          data::TCached,
+                          data_path::AbstractString,
+                          dependent_files
+                        )::CachedData{TCached} where {TCached}
+    return CachedData(
+        data,
+        # Get the "last-modified" time for each dependent file, plus the main one.
+        let paths = tuple((get_cached_path(fc, p) for p in (data_path, dependent_files...)...)),
+            file_modified_time(p) = unix2datetime(state(p).mtime)
+            Dict(p=>file_modified_time(p) for p in paths)
+        end,
+        full_path, now(), rand(fc.check_interval_ms)
+    )
 end
 
-function check_disk_modifications!( fc::FileCacher{TCached},
-                                    output::Optional = nothing
-                                  )::Bool where {TCached}
+function check_disk_modifications!(fc::FileCacher{TCached})::Bool where {TCached}
     any_changes::Bool = false
 
     empty!(fc.buffer)
@@ -214,12 +140,23 @@ function check_disk_modifications!( fc::FileCacher{TCached},
                 push!(output, path)
             end
 
-            pooled_resources = close_cached_data(data)
-            new_cache = make_data_cache(fc, path, pooled_resources)
-            if exists(new_cache)
-                fc.files[path] = new_cache
+            delete!(fc.files, path)
+
+            # Load the new data (or a fallback if it throws).
+            result = try
+                fc.reload_response(path, data)
+            catch e
+                fc.error_response(path, e, catch_backtrace(), data)
+            end
+            (new_data, new_dependent_files) = if result isa Tuple{TCached, Any}
+                result
             else
-                delete!(fc.files, path)
+                (result, ())
+            end
+
+            # Replace the data in the cache.
+            if new_data isa TCached
+                fc.files[path] = make_data_cache(fc, new_data, path, new_dependent_files)
             end
         end
     end
@@ -230,8 +167,7 @@ end
 "
 Retrieves the given file data, using a cached version if available.
 
-If the file can't be loaded, and no fallback 'error' file was provided to the cacher,
-    then `nothing` is returned.
+Returns `nothing` if the file can't be loaded and no fallback error value was provided to the cacher.
 "
 function get_cached_data!(fc::FileCacher{TCached}, path::AbstractString)::Optional{TCached} where {TCached}
     full_path = get_cached_path(fc, path)
@@ -239,15 +175,31 @@ function get_cached_data!(fc::FileCacher{TCached}, path::AbstractString)::Option
     if haskey(fc.files, full_path)
         return fc.files[full_path]
     else
-        new_value = make_data_cache(fc, full_path)
-        if exists(new_value)
-            fc.files[full_path] = new_value
+        # Load the file data (or a fallback if it throws).
+        result = try
+            fc.reload_response(path, data)
+        catch e
+            fc.error_response(path, e, catch_backtrace(), data)
         end
-        return new_value
+        (file_data, dependent_files) = if result isa Tuple{TCached, Any}
+            result
+        else
+            (result, ())
+        end
+
+        # Put the data in the cache and then return it.
+        if file_data isa TCached
+            fc.files[full_path] = make_data_cache(fc, file_data, full_path, dependent_files)
+            return file_data
+        else
+            return nothing
+        end
     end
 end
 
 
 export FileCacher,
        load_uncached_data, close_cached_data, close_pooled_data,
-       get_cached_data, check_disk_modifications!
+       get_cached_data!, check_disk_modifications!
+
+println("#TODO: Unit tests for file_cacher.jl")
