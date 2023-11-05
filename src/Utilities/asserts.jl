4abfef4dd1c810de644b93daca4c6496f0d1629e
William Manning <manning.w27@gmail.com>
2023-11-05 18:08:37 -0500

    Improve doc-string for @make_toggleable_asserts

--- src/Utilities/asserts.jl
+++ src/Utilities/asserts.jl
@@ -14,22 +14,30 @@ export @bp_check
 
 
 """
-The ToggleableAsserts package allows you to call asserts
-   which can be toggled on or off by the JIT compiler.
-However, this means that all projects which use that package
-   are forced to keep all asserts on or off, as a group.
-This macro generates the ToggleableAsserts code on command,
-   so that you can toggle asserts for a specifc module without affecting other ones.
-In particular it generates the following (assuming the prefix `X`):
-  * `Xasserts_enabled()`, to test if asserts are enabled.
-        Redefine this function to enable/disable asserts.
-  * `@Xassert`, to do an assert.
-  * `@Xdebug()`, a boolean for whether or not asserts are enabled.
-  * `@Xdebug a [b]`, to evaluate 'a' if asserts are enabled, and optionally 'b' otherwise.
+This is an alternative to *ToggleableAsserts.jl*.
+That package allows you to call asserts which can be toggled on or off by the JIT compiler;
+    however all projects using the package must respect a single global flag.
 
-These generated items are not exported, as the whole goal is to keep the asserts internal.
-The asserts are disabled by default, so that release builds don't pay extra JIT cost.
-You should redefine them when running tests.
+This macro generates ToggleableAsserts-style code,
+    so that you can have multiple separate instances of ToggleableAsserts.
+B+ uses this to give each sub-module its own debug flag,
+    and you can use it to give your project its own debug flag as well.
+
+Invoking `@make_toggleable_asserts X_` generates the following:
+  * `@X_assert(condition, msg...)` performs a check if `X_asserts_enabled()`,
+       and throws an error if the check fails.
+  * `@X_debug()` evaluates to a boolean for whether or not asserts are enabled.
+  * `@X_debug a [b]`, to evaluate 'a' if asserts are enabled, and optionally 'b' otherwise.
+  * `X_asserts_enabled()` represents the debug flag; it's a function that returns the constant `false`.
+    * For debug mode, your scripts can redefine it to return `true` instead.
+
+These generated items are not exported by default.
+Additionally, it's configured for "release" mode by default
+    so that release builds don't pay the extra JIT cost.
+When running tests, redefine `X_asserts_enabled() = true`.
+
+Note that constants are not part of JIT recompilation,
+    so a `const` global should never be defined in terms of the above macros.
 """
 macro make_toggleable_asserts(prefix::Symbol)
     name_toggler = Symbol(prefix, "asserts_enabled")
